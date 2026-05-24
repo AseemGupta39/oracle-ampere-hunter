35da3dd45d3234dbca23615d3ec951b219d55ef0
Aseem Gupta <aseemguptajaipur@gmail.com>
2026-05-24 21:13:45 +0000

    add oracle ampere hunter

---
+++ retry.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+while true
+do
+  echo "Trying at $(date)..."
+
+  oci compute instance launch \
+    --compartment-id $OCI_COMPARTMENT_ID \
+    --availability-domain "$OCI_AD" \
+    --shape VM.Standard.A1.Flex \
+    --subnet-id $OCI_SUBNET_ID \
+    --assign-public-ip true \
+    --image-id $(oci compute image list \
+      --compartment-id $OCI_COMPARTMENT_ID \
+      --operating-system "Canonical Ubuntu" \
+      --operating-system-version "22.04" \
+      --shape VM.Standard.A1.Flex \
+      --query 'data[0].id' \
+      --raw-output) \
+    --shape-config '{"ocpus":1,"memoryInGBs":2}' \
+    --display-name ampere-free-test
+
+  if [ $? -eq 0 ]; then
+    echo "SUCCESS!"
+    exit 0
+  fi
+
+  echo "Retrying in 90 seconds..."
+  sleep 90
+done
