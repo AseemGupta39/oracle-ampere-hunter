ef34e94b1af768647ebabd58667ba3195bf58763
Aseem Gupta <aseemguptajaipur@gmail.com>
2026-05-25 03:09:26 +0530

    Refactor OCI instance launch command for clarity

--- retry.sh
+++ retry.sh
@@ -5,20 +5,20 @@ do
   echo "Trying at $(date)..."
 
   oci compute instance launch \
-    --compartment-id $OCI_COMPARTMENT_ID \
-    --availability-domain "$OCI_AD" \
+  --compartment-id "$OCI_COMPARTMENT_ID" \
+  --availability-domain "$OCI_AD" \
+  --shape VM.Standard.A1.Flex \
+  --subnet-id "$OCI_SUBNET_ID" \
+  --assign-public-ip true \
+  --shape-config '{"ocpus":1,"memoryInGBs":2}' \
+  --display-name ampere-free-test \
+  --image-id "$(oci compute image list \
+    --compartment-id "$OCI_COMPARTMENT_ID" \
+    --operating-system "Canonical Ubuntu" \
+    --operating-system-version "22.04" \
     --shape VM.Standard.A1.Flex \
-    --subnet-id $OCI_SUBNET_ID \
-    --assign-public-ip true \
-    --image-id $(oci compute image list \
-      --compartment-id $OCI_COMPARTMENT_ID \
-      --operating-system "Canonical Ubuntu" \
-      --operating-system-version "22.04" \
-      --shape VM.Standard.A1.Flex \
-      --query 'data[0].id' \
-      --raw-output) \
-      --shape-config ocpus=1,memory-in-gbs=2
-    --display-name ampere-free-test
+    --query 'data[0].id' \
+    --raw-output)"
 
   if [ $? -eq 0 ]; then
     echo "SUCCESS!"
