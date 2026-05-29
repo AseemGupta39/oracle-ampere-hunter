ef2c08cab461da98b21097848d38b4c8b130b18f
Aseem Gupta <aseemguptajaipur@gmail.com>
2026-05-30 00:24:38 +0530

    Enhance instance creation and autoscaling feedback
    Updated success messages and added autoscaling logic for OCI Ampere instance.

--- retry.sh
+++ retry.sh
@@ -25,13 +25,43 @@ do
     --image-id "ocid1.image.oc1.ap-singapore-1.aaaaaaaa7x4t3jzqnr5vjvp66yasqfb27gw3p4cge3vz5ylhrdge43fefeca"
 
   if [ $? -eq 0 ]; then
-    echo "SUCCESS!"
+    echo "SUCCESS! Tiny instance created."
 
     curl -s -X POST "https://ntfy.sh/$NTFY_TOPIC" \
-      -d "OCI Ampere instance created!"
-
+      -d "instance created now going for autoscaling"
+  
+    INSTANCE_ID=$(oci compute instance list \
+      --compartment-id "$OCI_COMPARTMENT_ID" \
+      --query 'data[0].id' \
+      --raw-output)
+  
+    echo "Trying immediate scale to 4 OCPU / 24 GB..."
+  
+    oci compute instance update \
+      --instance-id "$INSTANCE_ID" \
+      --shape-config '{"ocpus":4,"memoryInGBs":24}'
+  
+    if [ $? -eq 0 ]; then
+      MESSAGE="SUCCESS! Scaled to 4 OCPU / 24 GB 🚀"
+      curl -s -X POST "https://ntfy.sh/$NTFY_TOPIC" \
+        -d "autoscaling successfull u have 4 core 24 gb ram"
+    else
+      MESSAGE="Tiny instance created, scale-up failed. Keeping 1 OCPU / 2 GB."
+      curl -s -X POST "https://ntfy.sh/$NTFY_TOPIC" \
+        -d "autoscaling failed stay there"
+    fi
+  
     exit 0
-  fi
+fi
+
+  # if [ $? -eq 0 ]; then
+  #   echo "SUCCESS!"
+
+  #   curl -s -X POST "https://ntfy.sh/$NTFY_TOPIC" \
+  #     -d "OCI Ampere instance created!"
+
+  #   exit 0
+  # fi
 
   sleep 90
 done
